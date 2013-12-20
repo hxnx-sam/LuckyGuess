0e7b9e71a926c69f98fa0848ad0ee35d80aedfea
hxnx-sam <samantha.lycett@ed.ac.uk>
2013-12-20 16:36:27 +0000

    Updated comments

--- src/main/LuckyGuess.java
+++ src/main/LuckyGuess.java
@@ -16,6 +16,27 @@ import io.*;
 import inference.*;
 
 /**
+ *  This is LuckyGuess.
+ *  It performs ABC parameter estimation over SIR-like models
+ *  
+ *  Copyright (C) 2013  Samantha Lycett
+ *  
+ *  This file is part of LuckyGuess.
+ *
+ *  LuckyGuess is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  any later version.
+ *
+ *  LuckyGuess is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with LuckyGuess.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * 
  * LuckyGuess - this is the main class, it reads the configuration xmlfile and runs the inference
  * @author Samantha Lycett
  * @version 20 Dec 2013
